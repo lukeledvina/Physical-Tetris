7b4f4f957b123ff8e3001145d610d8ec49bddb9f
Luke Ledvina <lukeledvina19@gmail.com>
2024-08-26 13:04:35 -0400

    Prepare scripts

---
+++ Game/game.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+# game.gd manages all of the components under it, connecting them accordingly.
+
+# a block spawner component will need to spawn blocks and put them as children of
+# the PlayerController.
+# A signal will need to be sent to the PlayerController to know to grab control of the new block
