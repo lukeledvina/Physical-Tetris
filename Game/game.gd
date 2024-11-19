6866dd6e0a68939644c5ae3dee4368c1c05a1927
Luke Ledvina <lukeledvina19@gmail.com>
2024-11-19 09:12:08 -0500

    Make everything steady state before resuming production

--- Game/game.gd
+++ Game/game.gd
@@ -1,18 +1,10 @@
 extends Node2D
 
+
 # game.gd manages all of the components under it, connecting them accordingly.
 
 # a block spawner component will need to spawn blocks and put them as children of
 # the PlayerController.
 # A signal will need to be sent to the PlayerController to know to grab control of the new block
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
 
 # Use this script to connect the functionality of child components. Don't have it be full of a ton of different functionality
-
-=======
->>>>>>> parent of 0c75646 (Update game.gd)
-=======
->>>>>>> parent of 0566024 (Setup scripts)
