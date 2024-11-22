edab67d0d063d2d04d356f5c31cc79859264468f
Luke Ledvina <lukeledvina19@gmail.com>
2024-11-21 19:44:35 -0500

    Refactor block to prepare for consistent left/right checking on raycasts

--- Blocks/block.gd
+++ Blocks/block.gd
@@ -5,22 +5,22 @@ extends StaticBody2D
 @onready var east_container: Node2D = $RayCastContainer/EastRayCastContainer
 @onready var west_container: Node2D = $RayCastContainer/WestRayCastContainer
 
-var active_ray_cast_container: Node2D = south_container
-var rotation_index: int = 2
+var down_ray_cast_container: Node2D = south_container
+var down_index: int = 2
 var ray_cast_container_array: Array = [north_container, east_container, south_container, west_container]
 
 # these functions determine which ray cast will be active next
 func rotate_clockwise():
-	rotation_index += 1
-	if rotation_index > 3:
-		rotation_index = 0
-	active_ray_cast_container = ray_cast_container_array[rotation_index]
+	down_index += 1
+	if down_index > 3:
+		down_index = 0
+	down_ray_cast_container = ray_cast_container_array[down_index]
 
 func rotate_counter_clockwise():
-	rotation_index -= 1
-	if rotation_index < 0:
-		rotation_index = 3
-	active_ray_cast_container = ray_cast_container_array[rotation_index]
+	down_index -= 1
+	if down_index < 0:
+		down_index = 3
+	down_ray_cast_container = ray_cast_container_array[down_index]
 
 # checks if the raycasts pointing "down" are colliding with anything
 func check_raycast():
