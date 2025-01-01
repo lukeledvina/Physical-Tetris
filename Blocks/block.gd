67eaf6574759639556284276880043f8fb1b60af
Luke Ledvina <lukeledvina19@gmail.com>
2025-01-01 17:16:41 -0500

    blocks collision now works after rotation

--- Blocks/block.gd
+++ Blocks/block.gd
@@ -5,86 +5,84 @@ extends StaticBody2D
 @onready var east_container: Node2D = $RayCastContainer/EastRayCastContainer
 @onready var west_container: Node2D = $RayCastContainer/WestRayCastContainer
 
-@onready var south_raycasts: Array = south_container.get_children()
-@onready var north_raycasts: Array = north_container.get_children()
-@onready var east_raycasts: Array = east_container.get_children()
-@onready var west_raycasts: Array = west_container.get_children()
+# Initialize raycast arrays
+@onready var south_raycasts: Array = []
+@onready var north_raycasts: Array = []
+@onready var east_raycasts: Array = []
+@onready var west_raycasts: Array = []
 
-@onready var down_ray_cast_container: Array = south_raycasts
-@onready var right_ray_cast_container: Array = east_raycasts
-@onready var left_ray_cast_container: Array = west_raycasts
-var down_index: int = 2:
-	set(index):
-		if index > 3:
-			down_index = 0
-		elif index < 0:
-			down_index = 3
-		else:
-			down_index = index
-var right_index: int = 1:
-	set(index):
-		if index > 3:
-			right_index = 0
-		elif index < 0:
-			right_index = 3
-		else:
-			right_index = index
-var left_index: int = 3:
-	set(index):
-		if index > 3:
-			left_index = 0
-		elif index < 0:
-			left_index = 3
-		else:
-			left_index = index
+# Indices for rotation
+var down_index: int = 2
+var right_index: int = 1
+var left_index: int = 3
 
-var ray_cast_container_array: Array = [north_raycasts, east_raycasts, south_raycasts, west_raycasts]
+var ray_cast_container_array: Array = []
 
-# these functions determine which ray cast will be active next
+# Active raycast containers
+var down_ray_cast_container: Array
+var right_ray_cast_container: Array
+var left_ray_cast_container: Array
+
+func _ready():
+	# Populate raycast arrays
+	south_raycasts = south_container.get_children()
+	north_raycasts = north_container.get_children()
+	east_raycasts = east_container.get_children()
+	west_raycasts = west_container.get_children()
+
+	# Initialize raycast container array
+	ray_cast_container_array = [north_raycasts, east_raycasts, south_raycasts, west_raycasts]
+
+	# Set initial active containers
+	update_ray_cast_containers()
+
+	# Debugging outputs
+	print("Initial Containers:")
+	print("Down RayCasts:", down_ray_cast_container)
+	print("Right RayCasts:", right_ray_cast_container)
+	print("Left RayCasts:", left_ray_cast_container)
+
+# Update raycast containers after rotation
+func update_ray_cast_containers():
+	down_ray_cast_container = ray_cast_container_array[down_index]
+	right_ray_cast_container = ray_cast_container_array[right_index]
+	left_ray_cast_container = ray_cast_container_array[left_index]
+
+# Rotate clockwise
 func rotate_clockwise():
 	rotation_degrees += 90
-	down_index -= 1
-	right_index -= 1
-	left_index -= 1
-	down_ray_cast_container = ray_cast_container_array[down_index]
-	right_ray_cast_container = ray_cast_container_array[right_index]
-	left_ray_cast_container = ray_cast_container_array[left_index]
+	down_index = (down_index - 1) % 4
+	right_index = (right_index - 1) % 4
+	left_index = (left_index - 1) % 4
+	update_ray_cast_containers()
+	print("Rotated Clockwise:")
+	print("Down:", down_index, "Right:", right_index, "Left:", left_index)
 
+# Rotate counter-clockwise
 func rotate_counter_clockwise():
 	rotation_degrees -= 90
-	down_index += 1
-	right_index += 1
-	left_index += 1
-	down_ray_cast_container = ray_cast_container_array[down_index]
-	right_ray_cast_container = ray_cast_container_array[right_index]
-	left_ray_cast_container = ray_cast_container_array[left_index]
+	down_index = (down_index + 1) % 4
+	right_index = (right_index + 1) % 4
+	left_index = (left_index + 1) % 4
+	update_ray_cast_containers()
+	print("Rotated Counter-Clockwise:")
+	print("Down:", down_index, "Right:", right_index, "Left:", left_index)
 
-# checks if the raycasts pointing "down" are colliding with anything
-func can_move_down() -> bool: #works
-	var raycasts: Array = down_ray_cast_container
-	for raycast in raycasts:
+# Collision checking functions
+func can_move_down() -> bool:
+	for raycast in down_ray_cast_container:
 		if raycast.is_colliding():
 			return false
-		else:
-			continue
 	return true
-	
+
 func can_move_right() -> bool:
-	var raycasts: Array = right_ray_cast_container
-	for raycast in raycasts:
+	for raycast in right_ray_cast_container:
 		if raycast.is_colliding():
 			return false
-		else:
-			continue
-	return true
-	
-func can_move_left() -> bool:
-	var raycasts: Array = left_ray_cast_container
-	for raycast in raycasts:
-		if raycast.is_colliding():
-			return false
-		else:
-			continue
 	return true
 
-	
+func can_move_left() -> bool:
+	for raycast in left_ray_cast_container:
+		if raycast.is_colliding():
+			return false
+	return true
